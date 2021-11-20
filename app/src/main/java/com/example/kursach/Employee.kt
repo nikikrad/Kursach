39f05baa4a0064746aae8f5c9cc9d81184391c88
NiKiKRaD <nikikrad228@gmail.com>
2021-11-21 01:05:33 +0300

    one more adapter, + change color ricycleView

---
+++ app/src/main/java/com/example/kursach/Employee.kt
@@ -0,0 +1,7 @@
+package com.example.kursach
+
+data class Employee(
+    var Name: String,
+    var Surname: String,
+    var Lastname:String,
+    var Position: String)
--- app/src/main/java/com/example/kursach/EmployeeFragment.kt
+++ app/src/main/java/com/example/kursach/EmployeeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kursach.databinding.FragmentEmployeeBinding
 
 class EmployeeFragment: Fragment() {
@@ -26,6 +27,21 @@ class EmployeeFragment: Fragment() {
         binding.btnAddPosition.setOnClickListener {
             (activity as? MainActivity)?.openFragment(AddPositionFragment())
         }
+
+        var employeeList = listOf(
+            Employee("Алёша","Алексеев","Хуесос", "Директор"),
+            Employee("Никитос","Шуриков","Александрович", "Хуесос"),
+            Employee("Ден","Скурик","Хуесос", "Норм"),
+            Employee("Синяга","Максимов","Писосов", "Орун"),
+            Employee("Андрей","Алексеев","Маруевич", "Бомж"),
+            Employee("Маруев","Синягов","Синягов", "Менеджер"),
+            Employee("Алёша","Что","Хуесос", "Стажер"),
+            )
+
+        val EmployeeAdapter = EmployeeAdapter(employeeList)
+        binding.rvEmployee.layoutManager = LinearLayoutManager(activity?.applicationContext, LinearLayoutManager.VERTICAL, false)
+        binding.rvEmployee.adapter = EmployeeAdapter
+
     }
 
     override fun onDestroy() {
